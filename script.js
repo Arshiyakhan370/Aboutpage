e9231c0a1a342b791db6fd9698924812f3547da0
arshiyakhan <Arshiyakhan370@gmail.com>
2023-12-12 21:38:07 +0530

    add-section

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+  const smallNav = document.querySelector('.small-nav');
+  const burgerMenu = document.querySelector('.burger-menu');
+
+  smallNav.classList.toggle('open');
+  burgerMenu.classList.toggle('open');
+}
