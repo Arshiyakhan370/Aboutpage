deaa18bfc0e91ae8e71e6f94acd59d6ac7e2b02d
arshiyakhan <Arshiyakhan370@gmail.com>
2023-12-15 10:39:49 +0530

    changes

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function toggleMenu() {
+  const body = document.body;
   const smallNav = document.querySelector('.small-nav');
   const burgerMenu = document.querySelector('.burger-menu');
 
   smallNav.classList.toggle('open');
   burgerMenu.classList.toggle('open');
 
-  
   if (smallNav.classList.contains('open')) {
     smallNav.style.display = "block";
+    body.style.overflow = "hidden"; 
   } else {
     smallNav.style.display = "none";
+    body.style.overflow = "auto"; 
   }
 }
